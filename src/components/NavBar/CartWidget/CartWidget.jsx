ad437c7992931c5fbcd22df416210b79be63d646
Lucas Gragera <lucasgragera1@gmail.com>
2023-07-08 21:26:41 -0300

    PreEntrega1Gragera

---
+++ src/components/NavBar/CartWidget/CartWidget.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CartWidget.css'
+
+export const CartWidget = () => {
+    return(
+        <div className='carrito'>
+        <h5>0</h5>  
+        <img height={30} src="https://w7.pngwing.com/pngs/225/984/png-transparent-computer-icons-shopping-cart-encapsulated-postscript-shopping-cart-angle-black-shopping-thumbnail.png" alt="Carrito" />
+        
+        </div>  
+    );
+};
+
+export default CartWidget;
