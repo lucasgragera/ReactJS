80ded73272d8fa86757551d441add4c7e818d340
lucasgragera <127903874+lucasgragera@users.noreply.github.com>
2023-07-08 21:07:34 -0300

    Add files via upload

---
+++ components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import './ItemListContainer.css'
+import React from "react";
+
+const ItemListContainter = (greetings) => {
+    return (
+        <h2>{greetings.titulo}</h2>
+        );
+}
+
+export default ItemListContainter;
